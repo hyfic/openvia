f32118945d3b90dedf87e29d629d027d59cd2b46
fadhilsaheer <fadhilsaheer@gmail.com>
2026-06-29 09:10:26 +0530

    Initial Commit

---
+++ openvia/openviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  openviaApp.swift
+//  openvia
+//
+//  Created by Fadhil Saheer on 29/06/26.
+//
+
+import SwiftUI
+
+@main
+struct openviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
